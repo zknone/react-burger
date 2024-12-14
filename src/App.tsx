e995fbe6aed951e7cd11b3e7a915918cae26c4b6
Streltsov Ivan <principdereva@gmail.com>
2024-12-14 18:20:37 +0100

    feat: made interface fixed, rid off extra styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import './App.css'
-import { BurgerIngredients } from './components/burger-ingredients/burger-ingredients'
-import { Menu } from './components/menu/menu'
+import './App.css';
+import { BurgerIngredients } from './components/burger-ingredients/burger-ingredients';
+import { AppHeader } from './components/app-header/app-header';
 
 function App() {
-
   return (
     <>
-      <Menu/>
-      <div className='contentContainer pt-10'>
-        <BurgerIngredients/>
-        <BurgerIngredients/>
+      <AppHeader />
+      <div className="contentContainer pt-10">
+        <BurgerIngredients extraClass="ml-5" />
+        <BurgerIngredients />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
