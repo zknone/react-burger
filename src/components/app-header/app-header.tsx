c484475b93fc660a10f0619e0a5c1a960496588e
Streltsov Ivan <principdereva@gmail.com>
2024-12-14 18:21:09 +0100

    fix: renamed files according to tz

---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,48 @@
+import {
+  Button,
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './app-header.module.css';
+const AppHeader = () => {
+  return (
+    <div className={`${styles.menuWrapper} pt-4 pb-4`}>
+      <div className={styles.menuBackground}></div>
+      <ul className={styles.menu}>
+        <li className={styles.menuItem}>
+          <Button
+            extraClass={`${styles.menuButton} pl-5 pr-5 pt-4 pb-4`}
+            style={{ color: 'white' }}
+            type="secondary"
+            htmlType="button"
+          >
+            <BurgerIcon type="primary" />
+            Constructor
+          </Button>
+        </li>
+        <li className={styles.menuItem}>
+          <Button
+            extraClass={`${styles.menuButton} pl-5 pr-5 pt-4 pb-4`}
+            type="secondary"
+            htmlType="button"
+          >
+            <ListIcon type="secondary" /> Order feed
+          </Button>
+        </li>
+      </ul>
+      <Logo />
+      <Button
+        extraClass={`${styles.menuButton} pl-5 pr-5 pt-4 pb-4`}
+        type="secondary"
+        htmlType="button"
+      >
+        <ProfileIcon type="secondary" />
+        My Account
+      </Button>
+    </div>
+  );
+};
+
+export { AppHeader };
