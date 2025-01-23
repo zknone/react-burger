0d3c62afedf0864da8d43f8370e10db42db2eaa7
Streltsov Ivan <principdereva@gmail.com>
2025-01-23 14:33:27 +0100

    feat: installed router

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from './components/app';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
