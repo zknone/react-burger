e58b2aa980c81118c269bb17ed24b1ea1507a28a
Streltsov Ivan <principdereva@gmail.com>
2025-02-03 16:33:09 +0100

    feat: added login route

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,65 @@
-import { styles } from './login.module.css';
+import { useState } from 'react';
+import {
+  Button,
+  EmailInput,
+  PasswordInput,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './login.module.css';
+import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
-  return <div>123</div>;
+  const [form, setForm] = useState({ email: '', password: '' });
+
+  const handleChange = (e: { target: { name: string; value: string } }) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <h2 className={`${styles.modal_title} text text_type_main-medium`}>
+          Вход
+        </h2>
+        <EmailInput
+          placeholder="E-mail"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+        />
+        <PasswordInput
+          placeholder="Пароль"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+        />
+        <Button htmlType="submit">Войти</Button>
+      </form>
+      <div className={styles.line}>
+        <p className="text text_type_main-default text_color_inactive">
+          Вы — новый пользователь?
+        </p>
+        <Link
+          className={`${styles.link} text text_type_main-default text`}
+          to="/register"
+        >
+          Зарегистрироваться
+        </Link>
+      </div>
+      <div className={styles.line}>
+        <p className="text text_type_main-default text_color_inactive">
+          Забыли пароль?
+        </p>
+        <Link
+          className={`${styles.link} text text_type_main-default text`}
+          to="/forgot-password"
+        >
+          Восстановить пароль
+        </Link>
+      </div>
+    </div>
+  );
 }
