c321e71dc464ce32232a9897b7271e0e298f93c0
Streltsov Ivan <principdereva@gmail.com>
2025-02-09 20:05:24 +0100

    feat: ingredient page

--- src/pages/ingredient/ingredient.tsx
+++ src/pages/ingredient/ingredient.tsx
@@ -1,21 +1,28 @@
 import { useParams } from 'react-router-dom';
 import { IngredientPopupDetails } from '../../components/burger-ingredients/ingredient-popup-details/ingredient-popup-details';
 import styles from './ingredient.module.css';
+import { useGetIngredientsQuery } from '../../services/api/ingredients-api/ingredients-api';
+import { IngredientType } from '../../types/types';
 
 export default function IngredientPage() {
   const { id } = useParams();
 
+  const { data } = useGetIngredientsQuery(undefined);
+  const ingredients: IngredientType[] = data?.data;
+
+  const foundIngredient = ingredients.filter((item) => item._id === id)[0];
+
   return (
     <div className={styles.container}>
       <h2 className={`${styles.modal_title} text text_type_main-large`}>
-        Заголовок {id}
+        {foundIngredient.name}
       </h2>
       <IngredientPopupDetails
-        img=""
-        protein={20}
-        calories={20}
-        carbs={20}
-        fat={20}
+        img={foundIngredient.image_large}
+        protein={foundIngredient.proteins}
+        calories={foundIngredient.calories}
+        carbs={foundIngredient.carbohydrates}
+        fat={foundIngredient.fat}
       />
     </div>
   );
