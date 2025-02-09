d4855878d5269f5e414d3ea744be6dc7c1530ba6
Streltsov Ivan <principdereva@gmail.com>
2025-02-09 19:56:59 +0100

    feat: added profileSlice

--- src/services/slices/profile/reducers.tsx
+++ src/services/slices/profile/reducers.tsx
@@ -1,11 +1,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ProfileResponse } from '../../../types/types';
 
-type Profile = { name: string; email: string };
+type Profile = {
+  profile: { name: string; email: string };
+  isAuthChecked: boolean;
+};
 
 const initialState: Profile = {
-  name: '',
-  email: '',
+  profile: {
+    name: '',
+    email: '',
+  },
+  isAuthChecked: false,
 };
 
 const profileSlice = createSlice({
@@ -13,14 +19,18 @@ const profileSlice = createSlice({
   initialState,
   reducers: {
     getProfile: (state, action: PayloadAction<ProfileResponse>) => {
-      state.email = action.payload.user.email;
-      state.name = action.payload.user.name;
+      state.profile.email = action.payload.user.email;
+      state.profile.name = action.payload.user.name;
     },
     changeProfile: (state, action: PayloadAction<Profile>) => {
       state = action.payload;
     },
+    setIsAuthChecked: (state, action: PayloadAction<boolean>) => {
+      state.isAuthChecked = action.payload;
+    },
   },
 });
 
-export const { getProfile, changeProfile } = profileSlice.actions;
+export const { getProfile, changeProfile, setIsAuthChecked } =
+  profileSlice.actions;
 export default profileSlice.reducer;
