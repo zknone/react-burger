9846a0597e12092cfe8b4f0c5f73fcfdf697e9dd
Streltsov Ivan <principdereva@gmail.com>
2026-01-06 19:55:20 +0100

    feat: logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,3 @@
-// Simple centralized logger to keep console noise consistent.
 export const logError = (context: string, error: unknown) => {
-  // eslint-disable-next-line no-console
   console.error(`[${context}]`, error);
 };
